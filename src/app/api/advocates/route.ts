510dc28f0dbe1e8ba071a5fb5bc9fb6ef1c14187
Grygoree <not.gregs.other.email@gmail.com>
2025-03-24 22:58:58 -0500

    Add paging to GET advocates route

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,12 +1,25 @@
+import { off } from "process";
 import db from "../../../db";
 import { advocates } from "../../../db/schema";
 import { advocateData } from "../../../db/seed/advocates";
 
-export async function GET() {
+const ITEMS_PER_PAGE = 3;
+
+/**
+ * 
+ * Uses offset+limit pagination to get advocates from db.
+ */
+export async function GET(request: Request) {
   // Uncomment this line to use a database
   // const data = await db.select().from(advocates);
+  const { searchParams } = new URL(request.url);
+  const page = parseInt(searchParams.get('page') || '0', 10);
 
   const data = advocateData;
+  const offset = page * ITEMS_PER_PAGE;
+  const end = offset + ITEMS_PER_PAGE;
 
-  return Response.json({ data });
+  const paginatedData = data.slice(offset, end);
+
+  return Response.json({ data: paginatedData });
 }
